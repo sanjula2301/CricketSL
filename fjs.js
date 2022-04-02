540871c52cb25e3a1ebeeb7b6792d1f79ae8e37a
sanjula2301 <90439186+sanjula2301@users.noreply.github.com>
2022-04-02 23:35:42 +0530

    Add files via upload
    1st uploard

---
+++ fjs.js
@@ -0,0 +1,14 @@
+//writes group members
+
+	var seconds=0;
+	function displayseconds(){
+	        seconds += 1;
+					
+	}
+	setInterval(displayseconds,1000);
+	
+	function redirectpage(){
+	        window.location="Sanjula Student2/main page/mainpage-1 (1).html";
+	}
+	
+	setTimeout('redirectpage()',4000);
